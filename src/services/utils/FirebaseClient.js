89986c39c2b44ab8601742392d964004599a74d1
Daniel Filho <danbsilva98@gmail.com>
2023-01-11 16:30:47 -0300

    Add firebase client

---
+++ src/services/utils/FirebaseClient.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+class FirebaseClient {
+  constructor() {
+    this.firebaseConfig = {
+      apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+      authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+      projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+      storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+      appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    };
+    this.app = initializeApp(this.firebaseConfig);
+    this.auth = getAuth(this.app);
+    this.db = getFirestore(this.app);
+  }
+}
+
+export default FirebaseClient;
